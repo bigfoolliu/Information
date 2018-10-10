0229e99c599e86ff6b130e0a3e52170dc2dec845
bigfoolliu <liu15671677014@icloud.com>
2018-10-10 15:37:48 +0800

    添加显示子评论实现

--- info/static/news/js/detail.js
+++ info/static/news/js/detail.js
@@ -193,9 +193,87 @@ $(function(){
             }
         }
 
+        // 发布评论(子评论)
         if(sHandler.indexOf('reply_sub')>=0)
         {
-            alert('回复评论')
+            var $this = $(this);
+            // 新闻id
+            var news_id = $this.parent().attr('data-newsid');
+            // 父评论id
+            var parent_id = $this.parent().attr('data-commentid');
+            // 评论内容
+            var comment = $this.prev().val();
+
+            // 若评论内容为空
+            if(!comment){
+                alert('请输入评论内容!');
+                return
+            }
+
+            // 组织请求参数
+            var params = {
+                'news_id': news_id,
+                'comment': comment,
+                'parent_id': parent_id
+            }
+
+            $.ajax({
+                url: '/news/news_comment',
+                type: 'POST',
+                contentType: 'application/json',
+                headers: {
+                    'X-CSRFToken': getCookie('csrf_token')
+                },
+                data: JSON.stringify(params),
+                success: function (resp) {
+                    // 子评论评论成功
+                    if(resp.erron == '0'){
+                        var comment = resp.data;
+                        // 拼接内容
+                        var comment_html = "";
+                        comment_html += '<div class="comment_list">';
+                        comment_html += '<div class="person_pic fl">';
+                        if (comment.user.avatar_url) {
+                            comment_html += '<img src="' + comment.user.avatar_url + '" alt="用户图标">'
+                        }else {
+                            comment_html += '<img src="../../static/news/images/person01.png" alt="用户图标">'
+                        }
+                        comment_html += '</div>';
+                        comment_html += '<div class="user_name fl">' + comment.user.nick_name + '</div>';
+                        comment_html += '<div class="comment_text fl">';
+                        comment_html += comment.content;
+                        comment_html += '</div>';
+                        comment_html += '<div class="reply_text_con fl">';
+                        comment_html += '<div class="user_name2">' + comment.parent.user.nick_name + '</div>';
+                        comment_html += '<div class="reply_text">';
+                        comment_html += comment.parent.content;
+                        comment_html += '</div>';
+                        comment_html += '</div>';
+                        comment_html += '<div class="comment_time fl">' + comment.create_time + '</div>';
+
+                        comment_html += '<a href="javascript:;" class="comment_up fr" data-commentid="' + comment.id + '" data-newsid="' + comment.news_id + '">赞</a>';
+                        comment_html += '<a href="javascript:;" class="comment_reply fr">回复</a>';
+                        comment_html += '<form class="reply_form fl" data-commentid="' + comment.id + '" data-newsid="' + news_id + '">';
+                        comment_html += '<textarea class="reply_input"></textarea>';
+                        comment_html += '<input type="button" value="回复" class="reply_sub fr">';
+                        comment_html += '<input type="reset" name="" value="取消" class="reply_cancel fr">';
+                        comment_html += '</form>';
+
+                        comment_html += '</div>';
+
+                        // 拼接到内容前面
+                        $('.comment_list_con').prepend(comment_html);
+                        // 清空输入框
+                        $this.prev().val();
+                        // 关闭子评论框
+                        $this.parent().hide();
+                        // 更新评论条数
+                        updateCommentCount()
+                    }else{
+                        alert(resp.errmsg)
+                    }
+                }
+            })
         }
 
     });
