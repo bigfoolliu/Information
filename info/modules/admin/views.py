834c399ad696aac0c5bb7cbef0ccbff656c1afa8
bigfoolliu <liu15671677014@icloud.com>
2018-10-14 12:26:47 +0800

    后台管理新闻审核待审核页面前后端实现

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -12,13 +12,73 @@ import time
 from flask import request, render_template, session, redirect, url_for, current_app, g, jsonify
 
 from info import db, constants
-from info.models import User
+from info.models import User, News
 from info.modules.admin import admin_bp
 
 from info.utils.common import user_login_data
 
 
 from info.utils.response_code import RET
+
+
+# 127.0.0.1:5000/admin/news_review_detail?news_id=新闻id
+@admin_bp.route('/news_review_detail')
+def news_review_detail():
+	"""
+	新闻审核细节后端接口
+	:return:
+	"""
+	return render_template('admin/news_review_detail.html')
+
+
+# 127.0.0.1:5000/admin/news_review?p=页码
+@admin_bp.route('/news_review')
+def news_review():
+	"""
+	新闻审核后端接口
+	:return:
+	"""
+	p = request.args.get('p', 1)
+	try:
+		p = int(p)
+	except Exception as e:
+		current_app.logger.error(e)
+		p = 1
+
+	news_list = []
+	current_page = 1
+	total_page = 1
+	# 获取查询关键字
+	keywords = request.args.get('keywords')
+	# 查询条件列表
+	filters = [News.status != 0]  # 审核的新闻状态不能为'通过'
+
+	if keywords:
+		# 如果是关键字查询,该新闻应该包含该关键字
+		filters.append(News.title.contains(keywords))
+	try:
+		paginate = News.query.filter(*filters).order_by(News.create_time.desc())\
+			.paginate(p, constants.ADMIN_NEWS_PAGE_MAX_COUNT, False)
+		news_list = paginate.items
+		current_page = paginate.page
+		total_page = paginate.pages
+	except Exception as e:
+		current_app.logger.error(e)
+		return jsonify(erron=RET.DBERR, errmsg='数据库查询新闻分页错误')
+
+	# 模型对象转字典列表
+	news_dict_list = []
+	for news in news_list if news_list else []:
+		news_dict_list.append(news.to_review_dict())
+
+	# 组织响应数据
+	data = {
+		'news_list': news_dict_list,
+		'current_page': current_page,
+		'total_page': total_page
+	}
+
+	return render_template('admin/news_review.html', data=data)
 
 
 # 127.0.0.1:5000/admin/user_list?p=页码
