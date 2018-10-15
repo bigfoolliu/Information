5e17b4bfb3e7bdb71e7a511b8152f17b23d76f81
bigfoolliu <liu15671677014@icloud.com>
2018-10-15 11:16:42 +0800

    后台管理新闻分类管理前后端实现

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -21,6 +21,35 @@ from info.utils.pic_storage import pic_storage
 from info.utils.response_code import RET
 
 
+# 127.0.0.1:5000/admin/news_type
+@admin_bp.route('/news_type')
+def news_type():
+	"""
+	新闻分类页面展示后端接口
+	:return:
+	"""
+	# 获取分类数据
+	try:
+		categories = Category.query.all()
+	except Exception as e:
+		current_app.logger.error(e)
+		return jsonify(erron=RET.DBERR, errmsg='数据库查询分类数据异常')
+
+	# 列表对象转字典
+	category_dict_list = []
+	for category in categories if categories else []:
+		category_dict = category.to_dict()
+		category_dict_list.append(category_dict)
+
+	# 删除最新分类
+	category_dict_list.pop(0)
+
+	data = {
+		'categories': category_dict_list
+	}
+	return render_template('admin/news_type.html', data=data)
+
+
 # 127.0.0.1:5000/admin/news_edit_detail?p=页码
 @admin_bp.route('/news_edit_detail', methods=['GET', 'POST'])
 def news_edit_detail():
